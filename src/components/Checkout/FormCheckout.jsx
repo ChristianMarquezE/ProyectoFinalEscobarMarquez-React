a9903e71507cc94777c707361541c741a4035503
Christian M <christianescobarmarquez@gmail.com>
2025-01-29 21:14:42 -0300

    Desabilitar botón si no clickea un botón para compartir

--- src/components/Checkout/FormCheckout.jsx
+++ src/components/Checkout/FormCheckout.jsx
@@ -10,8 +10,9 @@ export default function Form() {
     age: '',
     email: '',
   });
-  
+
   const [orderID, setOrderID] = useState(null); // New state for order ID visibility
+  const [isContacted, setIsContacted] = useState(false); // New state for button clicks
 
   function onInputChange(evt) {
     const inputName = evt.target.name;
@@ -30,14 +31,14 @@ export default function Form() {
         Nombre: userData.username,
         Apellido: userData.surname,
         Edad: userData.age,
-        Email: userData.email
+        Email: userData.email,
       },
       Fecha: new Date(),
       Artículos: cartItems,
-      total: getTotalPrice()
+      total: getTotalPrice(),
     };
     setOrderID(null);
-    const newOrderID = await createBuyOrder(orderData); 
+    const newOrderID = await createBuyOrder(orderData);
     setOrderID(newOrderID); // Set the order ID to state
   }
 
@@ -48,10 +49,8 @@ export default function Form() {
 
   return (
     <div className="form-checkout">
-    
-      
       <form>
-      <h2 className='form-group'>Para comprar, rellene sus datos:</h2>
+        <h2 className="form-group">Para comprar, rellene sus datos:</h2>
         <div className="form-group">
           <label htmlFor="username">Nombre</label>
           <input
@@ -78,22 +77,51 @@ export default function Form() {
         </div>
 
         <button
-          disabled={!(userData.username && userData.surname && userData.age && userData.email)}
+          disabled={
+            !(
+              userData.username &&
+              userData.surname &&
+              userData.age &&
+              userData.email
+            )
+          }
           onClick={handleCheckout}
         >
           Realizar Compra
         </button>
       </form>
 
-      {/* Conditional rendering for the order confirmation ticket */}  
+      {/* Conditional rendering for the order confirmation ticket */}
       {orderID && (
         <div className="order-confirmation">
-          <h3>Comparte tu ID de pedido, estás a un paso de terminar tu compra:</h3>
-          <p>Tu ID de pedido es: <strong>{orderID}</strong></p>
-          <p>Compartelo acá:</p>
-          <button onClick={() => window.open(`mailto:jorregor@udd.cl?subject=Mi ID de pedido&body=Mi ID de pedido es: ${orderID}`)}>Email</button>
-          <button onClick={() => window.open(`https://www.instagram.com/jorregodesign/`)}>Instagram</button>
-          <button onClick={handleReturnHome}>Volver al Inicio</button>
+          <h3>¡ESTAS A UN PASO DE FINALIZAR TU COMPRA!</h3>
+          <p>Comparte tu ID de compra:</p>
+          <p>
+            ID N°: <strong>{orderID}</strong>
+          </p>
+
+          <button
+            onClick={() => {
+              window.open(
+                `mailto:jorregor@udd.cl?subject=Mi ID de pedido&body=Mi ID de pedido es: ${orderID}`
+              );
+              setIsContacted(true); // Set state to true when email button is clicked
+            }}
+          >
+            Email
+          </button>
+          <button
+            onClick={() => {
+              window.open(`https://www.instagram.com/jorregodesign/`);
+              setIsContacted(true); // Set state to true when Instagram button is clicked
+            }}
+          >
+            Instagram
+          </button>
+          <p>El vendedor se comunicará a la brevedad</p>
+          <button onClick={handleReturnHome} disabled={!isContacted}>
+            Volver al Inicio
+          </button>
         </div>
       )}
     </div>
