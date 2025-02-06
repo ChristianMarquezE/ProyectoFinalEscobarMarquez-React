0b0668bb9864874a77ddbdb0ba1d4115decd3c04
Christian M <christianescobarmarquez@gmail.com>
2025-02-06 14:40:19 -0300

    rendering condicional en Ver Ticket

--- src/components/Ticket/TicketComponent.jsx
+++ src/components/Ticket/TicketComponent.jsx
@@ -22,38 +22,47 @@ export default function TicketComponent() {
 
   return (
     <div className="order-confirmation margin-top">
-      <h3>¡ESTAS A UN PASO DE FINALIZAR TU COMPRA!</h3>
-      <p>Comparte tu ID de compra:</p>
-      <p>
-        ID N°: <strong>{ticketid}</strong>
-      </p>
+      {ticketid === 'null' ? (
+        <>
+          <h3>No has realizado una compra aún.</h3>
+          <p>Podrás ver tu ticket una vez realizada la compra.</p>
+        </>
+      ) : (
+        <>
+          <h3>¡ESTAS A UN PASO DE FINALIZAR TU COMPRA!</h3>
+          <p>Comparte tu ID de compra:</p>
+          <p>
+            ID N°: <strong>{ticketid}</strong>
+          </p>
 
-      <button
-        onClick={() => {
-          window.open(
-            `mailto:jorregor@udd.cl?subject=Mi ID de pedido&body=Mi ID de pedido es: ${ticketid}`
-          );
-          setIsContacted(true);
-        }}
-      >
-        Email
-      </button>
-      <button
-        onClick={() => {
-          window.open(`https://www.instagram.com/jorregodesign/`);
-          setIsContacted(true);
-        }}
-      >
-        Instagram
-      </button>
-      <p>El vendedor se comunicará a la brevedad.</p>
-      <button
-        className={isContacted ? '' : 'button-disabled'}
-        onClick={handleReturnHome}
-        disabled={!isContacted}
-      >
-        Volver al Inicio
-      </button>
+          <button
+            onClick={() => {
+              window.open(
+                `mailto:jorregor@udd.cl?subject=Mi ID de pedido&body=Mi ID de pedido es: ${ticketid}`
+              );
+              setIsContacted(true);
+            }}
+          >
+            Email
+          </button>
+          <button
+            onClick={() => {
+              window.open(`https://www.instagram.com/jorregodesign/`);
+              setIsContacted(true);
+            }}
+          >
+            Instagram
+          </button>
+          <p>El vendedor se comunicará a la brevedad.</p>
+          <button
+            className={isContacted ? '' : 'button-disabled'}
+            onClick={handleReturnHome}
+            disabled={!isContacted}
+          >
+            Volver al Inicio
+          </button>
+        </>
+      )}
     </div>
   );
 }
