2ab7214ac0b980384dd1a0aa6b34efdb8485e652
Chris Márquez <christianescobarmarquez@gmail.com>
2026-04-27 20:21:35 -0400

    prueba navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import Nav from './components/NavBar/Nav/Nav';
-import Li from './components/NavBar/Li/Li';
-import NavLinks from './components/NavBar/NavLinks/NavLinks';
-import MenuPlegable from './components/NavBar/MenuPlegable/MenuPlegable';
-import Header from './components/NavBar/Header/Header';
-import HeaderTitle from './components/NavBar/HeaderTitle/HeaderTitle';
-import SocialIcons from './components/NavBar/SocialIcons/SocialIcons';
-import './App.css';
-import ItemListContainer from './components/ItemList/ItemListContainer';
-import ItemDetailContainer from './components/ItemDetail/ItemDetailContainer';
-import TicketComponent from './components/Ticket/TicketComponent';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Link, NavLink } from 'react-router-dom';
-import { CartContextProvider } from './components/Context/cartContext';
-import { TicketContextProvider } from './components/Context/ticketContext';
-import CartContainer from './components/CartWidget/CartContainer';
-import Contacto from './components/Contacto/Contacto';
+import Nav from "./components/NavBar/Nav/Nav";
+import Li from "./components/NavBar/Li/Li";
+import NavLinks from "./components/NavBar/NavLinks/NavLinks";
+import MenuPlegable from "./components/NavBar/MenuPlegable/MenuPlegable";
+import Header from "./components/NavBar/Header/Header";
+import HeaderTitle from "./components/NavBar/HeaderTitle/HeaderTitle";
+import SocialIcons from "./components/NavBar/SocialIcons/SocialIcons";
+import "./App.css";
+import ItemListContainer from "./components/ItemList/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetail/ItemDetailContainer";
+import TicketComponent from "./components/Ticket/TicketComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { CartContextProvider } from "./components/Context/cartContext";
+import { TicketContextProvider } from "./components/Context/ticketContext";
+import CartContainer from "./components/CartWidget/CartContainer";
+import Contacto from "./components/Contacto/Contacto";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
@@ -24,37 +26,70 @@ function App() {
           <BrowserRouter
             future={{ v7_relativeSplatPath: true, v7_startTransition: true }}
           >
-            <Header>
-              <Link to="/">
-                <HeaderTitle />
-              </Link>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+              <Container>
+                {/* Logo/Título envuelto en Link de react-router-dom */}
+                <Navbar.Brand as={Link} to="/">
+                  <HeaderTitle />
+                </Navbar.Brand>
 
-              <Nav>
-                <NavLinks>
-                  <Li texto="Inicio" to="/"></Li>
-                  <div className="colecciones">
-                    <NavLink to="/" className="colecciones-link">
-                      Colecciones
-                    </NavLink>
-                    <MenuPlegable>
-                      <Li texto="Praia" to="/coleccion/praia" />
-                      <Li texto="Éclat Céleste" to="/coleccion/eclat-celeste" />
-                      <Li
-                        texto="Resplandor del desierto"
+                {/* Botón Hamburguesa */}
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+                {/* Contenido colapsable */}
+                <Navbar.Collapse id="responsive-navbar-nav">
+                  <Nav className="me-auto">
+                    <Nav.Link as={Link} to="/">
+                      Inicio
+                    </Nav.Link>
+
+                    {/* Dropdown de Colecciones (reemplaza a MenuPlegable) */}
+                    <NavDropdown
+                      title="Colecciones"
+                      id="collasible-nav-dropdown"
+                    >
+                      <NavDropdown.Item as={Link} to="/coleccion/praia">
+                        Praia
+                      </NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/coleccion/eclat-celeste">
+                        Éclat Céleste
+                      </NavDropdown.Item>
+                      <NavDropdown.Item
+                        as={Link}
                         to="/coleccion/resplandor-del-desierto"
-                      />
-                      <Li texto="SastrO" to="/coleccion/sastro" />
-                      <Li texto="Retrofutura" to="/coleccion/retrofutura" />
-                      <Li texto="Ver Todo" to="/" />
-                    </MenuPlegable>
+                      >
+                        Resplandor del desierto
+                      </NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/coleccion/sastro">
+                        SastrO
+                      </NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/coleccion/retrofutura">
+                        Retrofutura
+                      </NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item as={Link} to="/">
+                        Ver Todo
+                      </NavDropdown.Item>
+                    </NavDropdown>
+
+                    <Nav.Link as={Link} to="/category/joyeria">
+                      Joyería
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/category/vestidos">
+                      Vestidos
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/category/jumpsuits">
+                      Jumpsuits
+                    </Nav.Link>
+                  </Nav>
+
+                  {/* Iconos Sociales al final */}
+                  <div className="d-flex">
+                    <SocialIcons />
                   </div>
-                  <Li texto="Joyería" to="category/joyeria" />
-                  <Li texto="Vestidos" to="category/vestidos" />
-                  <Li texto="Jumpsuits" to="category/jumpsuits" />
-                </NavLinks>
-              </Nav>
-              <SocialIcons />
-            </Header>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
 
             <Routes>
               <Route
