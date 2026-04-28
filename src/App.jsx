9dba75ad1ec0ef38d27b0431246a310b6974e6cd
Chris Márquez <christianescobarmarquez@gmail.com>
2026-04-27 20:35:37 -0400

    menu hamburguesa

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-// 1. Estilos
+import "bootstrap/dist/css/bootstrap.min.css"; // ¡Importante!
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-// 2. Componentes de Terceros (Librerías)
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
-import {
-  Navbar,
-  Nav as BootstrapNav,
-  NavDropdown,
-  Container,
-} from "react-bootstrap";
 
-// 3. Contextos
+// 1. Componentes de Bootstrap
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+
+// 2. React Router
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+
+// 3. Tus Contextos
 import { CartContextProvider } from "./components/Context/cartContext";
 import { TicketContextProvider } from "./components/Context/ticketContext";
 
-// 4. Tus Componentes Locales
-// NOTA: Si vas a usar el Nav de Bootstrap para el menú hamburguesa,
-// renombramos tu componente local para que no choque.
-import CustomNav from "./components/NavBar/Nav/Nav";
-import Li from "./components/NavBar/Li/Li";
-import NavLinks from "./components/NavBar/NavLinks/NavLinks";
-import MenuPlegable from "./components/NavBar/MenuPlegable/MenuPlegable";
-import Header from "./components/NavBar/Header/Header";
+// 4. Tus Componentes Locales (Importa solo los que uses)
 import HeaderTitle from "./components/NavBar/HeaderTitle/HeaderTitle";
 import SocialIcons from "./components/NavBar/SocialIcons/SocialIcons";
 import ItemListContainer from "./components/ItemList/ItemListContainer";
@@ -29,103 +19,104 @@ import ItemDetailContainer from "./components/ItemDetail/ItemDetailContainer";
 import TicketComponent from "./components/Ticket/TicketComponent";
 import CartContainer from "./components/CartWidget/CartContainer";
 import Contacto from "./components/Contacto/Contacto";
+
 function App() {
   return (
-    <>
-      <CartContextProvider>
-        <TicketContextProvider>
-          <BrowserRouter
-            future={{ v7_relativeSplatPath: true, v7_startTransition: true }}
-          >
-            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-              <Container>
-                {/* Logo/Título envuelto en Link de react-router-dom */}
-                <Navbar.Brand as={Link} to="/">
-                  <HeaderTitle />
-                </Navbar.Brand>
+    <CartContextProvider>
+      <TicketContextProvider>
+        <BrowserRouter
+          future={{ v7_relativeSplatPath: true, v7_startTransition: true }}
+        >
+          {/* NAVBAR REESTRUCTURADO */}
+          <Navbar expand="lg" className="bg-body-tertiary" collapseOnSelect>
+            <Container>
+              {/* Logo / Título */}
+              <Navbar.Brand as={Link} to="/">
+                <HeaderTitle />
+              </Navbar.Brand>
 
-                {/* Botón Hamburguesa */}
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              {/* Botón Hamburguesa */}
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-                {/* Contenido colapsable */}
-                <Navbar.Collapse id="responsive-navbar-nav">
-                  <Nav className="me-auto">
-                    <Nav.Link as={Link} to="/">
-                      Inicio
-                    </Nav.Link>
+              {/* Contenido colapsable */}
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link as={Link} to="/">
+                    Inicio
+                  </Nav.Link>
 
-                    {/* Dropdown de Colecciones (reemplaza a MenuPlegable) */}
-                    <NavDropdown
-                      title="Colecciones"
-                      id="collasible-nav-dropdown"
+                  {/* Reemplazo de MenuPlegable con NavDropdown */}
+                  <NavDropdown title="Colecciones" id="basic-nav-dropdown">
+                    <NavDropdown.Item as={Link} to="/coleccion/praia">
+                      Praia
+                    </NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/coleccion/eclat-celeste">
+                      Éclat Céleste
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      as={Link}
+                      to="/coleccion/resplandor-del-desierto"
                     >
-                      <NavDropdown.Item as={Link} to="/coleccion/praia">
-                        Praia
-                      </NavDropdown.Item>
-                      <NavDropdown.Item as={Link} to="/coleccion/eclat-celeste">
-                        Éclat Céleste
-                      </NavDropdown.Item>
-                      <NavDropdown.Item
-                        as={Link}
-                        to="/coleccion/resplandor-del-desierto"
-                      >
-                        Resplandor del desierto
-                      </NavDropdown.Item>
-                      <NavDropdown.Item as={Link} to="/coleccion/sastro">
-                        SastrO
-                      </NavDropdown.Item>
-                      <NavDropdown.Item as={Link} to="/coleccion/retrofutura">
-                        Retrofutura
-                      </NavDropdown.Item>
-                      <NavDropdown.Divider />
-                      <NavDropdown.Item as={Link} to="/">
-                        Ver Todo
-                      </NavDropdown.Item>
-                    </NavDropdown>
+                      Resplandor del desierto
+                    </NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/coleccion/sastro">
+                      SastrO
+                    </NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/coleccion/retrofutura">
+                      Retrofutura
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item as={Link} to="/">
+                      Ver Todo
+                    </NavDropdown.Item>
+                  </NavDropdown>
 
-                    <Nav.Link as={Link} to="/category/joyeria">
-                      Joyería
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/category/vestidos">
-                      Vestidos
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/category/jumpsuits">
-                      Jumpsuits
-                    </Nav.Link>
-                  </Nav>
+                  <Nav.Link as={Link} to="/category/joyeria">
+                    Joyería
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/category/vestidos">
+                    Vestidos
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/category/jumpsuits">
+                    Jumpsuits
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/contacto">
+                    Contacto
+                  </Nav.Link>
+                </Nav>
 
-                  {/* Iconos Sociales al final */}
-                  <div className="d-flex">
-                    <SocialIcons />
-                  </div>
-                </Navbar.Collapse>
-              </Container>
-            </Navbar>
+                {/* Iconos sociales o CartWidget */}
+                <div className="d-flex align-items-center">
+                  <SocialIcons />
+                </div>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
 
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <ItemListContainer greeting="Productos de Diseñadora Profesional:" />
-                }
-              />
-              <Route
-                path="/category/:catid"
-                element={<ItemListContainer greeting="Compras por categoría" />}
-              />
-              <Route
-                path="/coleccion/:colid"
-                element={<ItemListContainer greeting="Compras por colección" />}
-              />
-              <Route path="/item/:id" element={<ItemDetailContainer />} />
-              <Route path="/cart" element={<CartContainer />} />
-              <Route path="/contacto" element={<Contacto />} />
-              <Route path="/ticket/:ticketid" element={<TicketComponent />} />
-            </Routes>
-          </BrowserRouter>
-        </TicketContextProvider>
-      </CartContextProvider>
-    </>
+          {/* RUTAS (Sin cambios) */}
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ItemListContainer greeting="Productos de Diseñadora Profesional:" />
+              }
+            />
+            <Route
+              path="/category/:catid"
+              element={<ItemListContainer greeting="Compras por categoría" />}
+            />
+            <Route
+              path="/coleccion/:colid"
+              element={<ItemListContainer greeting="Compras por colección" />}
+            />
+            <Route path="/item/:id" element={<ItemDetailContainer />} />
+            <Route path="/cart" element={<CartContainer />} />
+            <Route path="/contacto" element={<Contacto />} />
+            <Route path="/ticket/:ticketid" element={<TicketComponent />} />
+          </Routes>
+        </BrowserRouter>
+      </TicketContextProvider>
+    </CartContextProvider>
   );
 }
 
